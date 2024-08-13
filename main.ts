3284f4aed051e71cad692866008c95698ea31c3d
Breawn Schoun <breawn.schoun@ucdenver.edu>
2024-08-13 10:21:20 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,331 @@
+namespace SpriteKind {
+    export const Background = SpriteKind.create()
+    export const Mountain = SpriteKind.create()
+}
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    effects.clearParticles(balloon)
+    balloon.startEffect(effects.fire)
+    balloon.startEffect(effects.fire)
+})
+function createCoin () {
+    if (heartCount == 0 || heartCount == breathHoldSeconds) {
+    	
+    } else if (heartCount < breathHoldSeconds) {
+        coin = sprites.createProjectileFromSide(img`
+            . . . . . b b b b b b . . . . . 
+            . . . b b 9 9 9 9 9 9 b b . . . 
+            . . b b 9 9 9 9 9 9 9 9 b b . . 
+            . b b 9 d 9 9 9 9 9 9 9 9 b b . 
+            . b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
+            b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
+            b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
+            b 9 3 9 9 9 9 9 9 9 9 9 1 9 9 b 
+            b 5 3 d 9 9 9 9 9 9 9 9 9 9 9 b 
+            b 5 3 3 9 9 9 9 9 9 9 9 9 d 9 b 
+            b 5 d 3 3 9 9 9 9 9 9 9 d d 9 b 
+            . b 5 3 3 3 d 9 9 9 9 d d 5 b . 
+            . b d 5 3 3 3 3 3 3 3 d 5 b b . 
+            . . b d 5 d 3 3 3 3 5 5 b b . . 
+            . . . b b 5 5 5 5 5 5 b b . . . 
+            . . . . . b b b b b b . . . . . 
+            `, -30, 0)
+        coin.y = 20
+    } else {
+        coin = sprites.createProjectileFromSide(img`
+            . . . . . b b b b b b . . . . . 
+            . . . b b 9 9 9 9 9 9 b b . . . 
+            . . b b 9 9 9 9 9 9 9 9 b b . . 
+            . b b 9 d 9 9 9 9 9 9 9 9 b b . 
+            . b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
+            b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
+            b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
+            b 9 3 9 9 9 9 9 9 9 9 9 1 9 9 b 
+            b 5 3 d 9 9 9 9 9 9 9 9 9 9 9 b 
+            b 5 3 3 9 9 9 9 9 9 9 9 9 d 9 b 
+            b 5 d 3 3 9 9 9 9 9 9 9 d d 9 b 
+            . b 5 3 3 3 d 9 9 9 9 d d 5 b . 
+            . b d 5 3 3 3 3 3 3 3 d 5 b b . 
+            . . b d 5 d 3 3 3 3 5 5 b b . . 
+            . . . b b 5 5 5 5 5 5 b b . . . 
+            . . . . . b b b b b b . . . . . 
+            `, -30, 0)
+        coin.y = scene.screenHeight() - 20
+    }
+    heartCount += 1
+    if (heartCount == breathHoldSeconds * 2) {
+        heartCount = 0
+    }
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    otherSprite.startEffect(effects.bubbles)
+    sprites.destroy(otherSprite)
+    info.changeScoreBy(1)
+})
+function placeMountain (leftPosition: number) {
+    lastCreatedMountain = sprites.create(mountains[randint(0, 1)], SpriteKind.Mountain)
+    lastCreatedMountain.setFlag(SpriteFlag.Ghost, true)
+    lastCreatedMountain.setFlag(SpriteFlag.AutoDestroy, true)
+    lastCreatedMountain.bottom = scene.screenHeight()
+    lastCreatedMountain.left = leftPosition
+    lastCreatedMountain.z = -15
+}
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    effects.clearParticles(balloon)
+    balloon.setImage(balloonDeflated)
+})
+let coin: Sprite = null
+let breathHoldSeconds = 0
+let lastCreatedMountain: Sprite = null
+let mountains: Image[] = []
+let balloon: Sprite = null
+let heartCount = 0
+let balloonDeflated: Image = null
+balloonDeflated = img`
+    ...................
+    ...................
+    .......fffff.......
+    .....ff22222ff.....
+    ....f222222442f....
+    ...f22222222442f...
+    ..f2222222222442f..
+    .f222322223222422f.
+    .f222322223222222f.
+    .f222322223222222f.
+    .f222232222322222f.
+    .f222222222222222f.
+    ..f2222322232222f..
+    ..f2222233322222f..
+    ...ff222222222ff...
+    ....ffff222ffff....
+    .....f.fffff.f.....
+    .....f.......f.....
+    .....f.......f.....
+    ......f.....f......
+    ......f.....f......
+    .......f...f.......
+    .......f.2.f.......
+    ......fffffff......
+    .....fcccccccf.....
+    .....fcbbbbbcf.....
+    .....fcabbbacf.....
+    .....fcbaaabcf.....
+    .....fcbbbbbcf.....
+    .....fcabbbacf.....
+    .....fccaaaccf.....
+    ......fcccccf......
+    .......fffff.......
+    `
+let balloonInflated = img`
+    ...................
+    ......fffffff......
+    ....ff2222222ff....
+    ...f22222222442f...
+    ..f2222222222442f..
+    .f222222222222442f.
+    f22232222223222422f
+    f22232222223222222f
+    f22232222223222222f
+    f22223222222322222f
+    f22222222222222222f
+    f22222222222222222f
+    .f222232222232222f.
+    .f222223333322222f.
+    ..ff22222222222ff..
+    ....ffff222ffff....
+    .....f.fffff.f.....
+    .....f.......f.....
+    .....f.......f.....
+    ......f.....f......
+    ......f.....f......
+    .......f...f.......
+    .......f.2.f.......
+    ......fffffff......
+    .....fcccccccf.....
+    .....fcbbbbbcf.....
+    .....fcabbbacf.....
+    .....fcbaaabcf.....
+    .....fcbbbbbcf.....
+    .....fcabbbacf.....
+    .....fccaaaccf.....
+    ......fcccccf......
+    .......fffff.......
+    `
+heartCount = 0
+music.setVolume(0)
+balloon = sprites.create(balloonDeflated, SpriteKind.Player)
+scene.setBackgroundColor(6)
+balloon.setStayInScreen(true)
+balloon.z = 100
+info.setScore(0)
+mountains = [img`
+    ......................333333........................3333........................
+    ...................333333333333...................3333333333....................
+    ................333333333333333333..............3333333333333333................
+    .............33333333333333333333333.........333333333333333333333333...........
+    ..........333333333333333333333333333......333333333333333333333333333333.......
+    ........3333333333333333333333333333333..33333333333333333333333333bb33333333...
+    ......333333333bb3333333333333333b33333b3333333333333333333333333bbbb333333333..
+    ....333333333333b33333333333333333b333bb3333333333333333333333bbbbbbbb333333333.
+    ..3333333333333bbb33b3333333333b33bbbbbbbb3b33333333333333b3bbbbbbbbbb3333333333
+    3333333333333333bb33b3333333333bbbbbbbbbbbbb33333333333333bbbbbbbbbbbbb3333b3333
+    3333333333333b33bbbbbb33b3b3bbbbbbbbbbbbbbbbb333333333b33bbbbbbbbbbbbbbbb33b3333
+    333333333333bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb3b333333bbbbbbbbbbbcbbbbbbbbbb3bb3
+    333333b33bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb333bbbbbbbbbcbbbcccbbbbbbbbb3b33
+    333b33bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbcbbcbbcccccbccbbbbbbbbbb33
+    333bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbccccccccccccccbccbbbbbbbbbb
+    33bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbcccccccccccccccccccccccbccbbbbbbcb
+    cbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbccccccbccccccccccccbcccccccccccccbcbbcc
+    cccbcbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbcbbcccbccccccccccccccccccbcccccccccccccbccccc
+    cccccbbcbbbbbbbbbbbbbbbbbbbbbbbbbcbcccccccccccccccccccccccccccccccccccccccbbcccc
+    ccccccccbcbcbbbbbbbbbbbbbbbbbbcbcccccccccccccccccccccccccccccccccccccccccccccccc
+    ccccccccccccbbcbbbbcbbbbbbbbbbcccccccccccccccccccccccccccccccccccccccccccccccccc
+    ccccccccccccccccbcbcbbbbbbcbcccccccccccccccccccccccccccccccccccccccccccccccccccc
+    ccccbcccccccccccccccccbcbbcccccccccbcccccccccccccccccccccccccccccccccccccccccccc
+    ccccccbcccccccccccccccccccccccccccbccccccccccccccccccccccccccccccccccccccccccccc
+    ccccbcccbcccbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+    ccccccccccbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+    cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+    cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+    cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+    cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+    cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+    cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+    `, img`
+    ....................3...........................................................
+    ....................3...........................................................
+    ...................333..........................................................
+    ...................3333.........................................................
+    ..................333333........................................................
+    ..................33333333......................................................
+    .................33333333333....................................................
+    ................33333333333333..................................................
+    ...............3333333333333333.......................3.........................
+    ..............333333333333333333...................33333........................
+    .............33333333333333333333...............333333333333....................
+    ...........33333333333333333333333............3333333333333333..................
+    ..........3333333333333333333333333..........3333333333333333333................
+    ........3333333333333333333333333333.......3333333333333333333b333..............
+    .....33333333333333333333333333333333...333333333333333333333bbb3333..........3.
+    .33333333333333333333333333333333333333333333333333333333333bbbbb33333......3333
+    33333333333333333333333333333333333333333333333333333333333333b33333333333333333
+    333333333333333333333333333333333b333333333333333333333333333bb33333333333333333
+    333333333333333333333333333333b3bbb3b3333333333333333333333bbbbb333333333b333333
+    333333333333b33333333333333333bbbbbbb3333333333333333333bbbbbbbbb33333333bb33333
+    333333333b33b33b33b333333b33bbbbbbbbbb333333333333bbbbbbbbbbbbbbb33333333bbb3333
+    33333333bbbbbbbbbbbb3b333bbbbbbbbbbbbbb33333333bbbbbbbbbbbbbbbbbbb333333bbbb33b3
+    3333b3bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb3333bbbbbbbbbbbbbbbbbbbbbbb3b3bbbbbbbbb
+    c333bbbbbbbbbbbbbbbbbbbbbbbbbbbcbcbbbbbbbbb3bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbc
+    cccbbbbbbbbbbbbbbbbbbbbbbbbbcbbccccbbbbbbbbbbbbbbbbbbbbcbbbbbbbbbbbbbbbbbbbcbbcc
+    ccccbbbbbbbbbbbbbbbbbbbbbbbccccccbccbcbcbbbbbbbbbbbcbbccbbbbbbbbbbbbbbbbbbbcbccc
+    ccccccbcbbbbbbbbbbbbbbbbbccccccccccccccccbbbbbbbcbbcccccccbcbbbbbbbbbbbbbcbccccc
+    ccccccccbcbbbbbccbbbbbbcccccccccccccccccccccbcbcccccccccccccbbcbbbbbbbbbbccccccc
+    cbccccccccbbcbccccbbcbcccccccccccccccccccccccccccccccccccccccccbbbbbcccccccccbcc
+    cccccccccccccbbccbbbcccccbcbcccccccccccccccccccccccccccbccccbccccccccccccccccccc
+    ccccccccccccccccccccccccccbcbcccccccccccccccccccccccbccccccccccccccccccccccccccc
+    ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccbccccc
+    cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+    ccccccccccccccccccccccccccccccccccccccccccccccbccccccccccccccccccccccccccccccccc
+    ccccccbcbcbcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccbccccccccc
+    ccccccccbcbccccccccccccccccccccccccccccccccccccccccccccccbcccccccccccccccccccccc
+    ccccccccccbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+    cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+    cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+    `]
+let sun = sprites.create(img`
+    ......................................................................444444444444444...........................................................................
+    ..............................................................444444444444444444444444444444444.................................................................
+    ........................................................44444444444444444444555555444444444444444444444.........................................................
+    .....................................................444444444444444444444555555555544444444444444444444444444444...............................................
+    ..................................................44444444444444444444445555555555555544444444444444444444444444444.............................................
+    ...............................................44444444444444444444444455555555555555554444444444444444444444444444444444444....................................
+    .......................................4444444444444444444444444444444455555555555555554444444444444444444444444444444444444444444444444........................
+    .................................444444444444444444444444444444444444455555555555555555544444444444444444444444444444444444444444444444444444444................
+    .........................44444444444444444444444444444444444444444444455555555555555555544444444444444444444444444444444444444444444444444444444444444444.......
+    44444.......444444444444444444444444444444444444444444444444444444444555555555555555555554444444444444444444444444444444444444444444444444444444444444444444....
+    444444444444444444444444444444444444444444444444444444444444444444444555555555555555555554444444444444444444444444444444444444444444444444444444444444444444444.
+    4444444444444444444444444444444444444444444444444444444444444444444445555555555555555555544444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444445555555555555555555544444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444445555555555555555555544444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444444555555555555555555444444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444444555555555555555555444444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444444455555555555555554444444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444444455555555555555554444444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444444445555555555555544444444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444444444455555555554444444444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444444444444555555444444444444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
+    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
+    `, SpriteKind.Background)
+sun.bottom = scene.screenHeight()
+sun.setFlag(SpriteFlag.Ghost, true)
+sun.z = -20
+controller.moveSprite(balloon, 0, 75)
+placeMountain(0)
+placeMountain(lastCreatedMountain.right)
+game.setDialogFrame(img`
+    ...cc......................cc....
+    ..c55c..bbbb...bbbbb......c55c...
+    .cb55bcbdddbbbbbdddbbbbbbcb55bc..
+    b555555bbdddb111bdddb11db555555b.
+    bb5555bbdbdb11111bdb1111bb5555bb.
+    cb5555bcddd11111ddd11111cb5555bc.
+    .c5bb5c1111d111d111d111ddc5bb5c..
+    .cbbbbc111111111111111111cbbbbc..
+    ..b11111111111111111111111d111bb.
+    ..b111111111111111111111111d1bdb.
+    ..bb11111111111111111111111dbddb.
+    .bbdb1d11111111111111111111ddddb.
+    .bdddd11111111111111111111d1bdbb.
+    .bddbd11111111111111111111111bb..
+    .bdb1d111111111111111111111111b..
+    .bb111d11111111111111111111111b..
+    ..b11111111111111111111111d111bb.
+    ..b111111111111111111111111d1bdb.
+    ..bb11111111111111111111111dbddb.
+    .bbdb1d11111111111111111111ddddb.
+    .bdddd11111111111111111111d1bdbb.
+    .bddbd11111111111111111111111bb..
+    .bdbb1111111111111111111111111b..
+    .bbbd1111111111111111111111111b..
+    ..bcc111111111111111111111dccdb..
+    ..c55c111d111d111d111d1111c55cb..
+    .cb55bcdd11111ddd11111dddcb55bc..
+    b555555b11111bdb11111bdbb555555b.
+    bb5555bbb111bdddb111bdddbb5555bb.
+    cb5555bcdbbbbbdddbbbbbddcb5555bc.
+    .c5bb5c.bb...bbbbb...bbbbc5bb5c..
+    .cbbbbc..................cbbbbc..
+    .................................
+    `)
+game.showLongText("Use belly breathing and breath holds to move the hot air balloon!", DialogLayout.Center)
+game.showLongText("Hold the Puck on your belly, soft side up.", DialogLayout.Center)
+game.showLongText("Inhale to make the balloon go up, exhale to make it go down.", DialogLayout.Center)
+game.showLongText("Pop as many bubbles as you can!", DialogLayout.Center)
+breathHoldSeconds = game.askForNumber("Breath hold length:", 2) + 1
+game.onUpdateInterval(1000, function () {
+    createCoin()
+})
+game.onUpdateInterval(200, function () {
+    // changing position explicitly to avoid fractions of
+    // movement / clipping
+    for (let value of sprites.allOfKind(SpriteKind.Mountain)) {
+        value.x += -1
+    }
+    if (lastCreatedMountain.right < scene.screenWidth()) {
+        placeMountain(lastCreatedMountain.right)
+    }
+})
